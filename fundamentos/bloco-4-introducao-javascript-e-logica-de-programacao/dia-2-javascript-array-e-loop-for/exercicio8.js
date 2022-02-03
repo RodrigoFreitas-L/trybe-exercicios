4418fd549160983e1906da04a2fc0f2074367a5d
Rodrigo Freitas <kdigo.chr@gmail.com>
2022-02-03 20:49:59 -0300

    Exercicio 9 finalizado

---
+++ fundamentos/bloco-4-introducao-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio8.js
@@ -0,0 +1,19 @@
+let array25 = [];
+// let dividedArray = [];
+
+for (let i = 1; i <= 25; i += 1) {
+    array25.push(i);
+}
+
+// for (let i = 0; i < array25.length; i += 1) {
+//     dividedArray[i] = array25[i] / 2;
+// }
+
+// console.log(dividedArray);
+
+///// testando a resposta do gabarito
+
+for (let i2 = 0; i2 < array25.length; i2 += 1) {
+    console.log(array25[i2] / 2);
+
+}
