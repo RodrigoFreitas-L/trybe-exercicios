a93544259dc3979c975a4ea51c011bc80855c8e2
Rodrigo Freitas <kdigo.chr@gmail.com>
2022-03-14 12:18:09 -0300

    Alguns exemplos de resolucao adicionados

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-higher-order-functions-map-filter/exercise5.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-higher-order-functions-map-filter/exercise5.js
@@ -1,12 +1,5 @@
 const books = require('./obj');
 
-const expectedResult = [
-  'Frank Herbert',
-  'George R. R. Martin',
-  'Isaac Asimov',
-  'J. R. R. Tolkien',
-];
-
 let obj = [];
 
 const sciFiOrFantasyAuthors = () => books.filter((element) => (element.genre === 'Fantasia' || element.genre === 'Ficção Científica')).map((element) => element.author.name);
--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-higher-order-functions-map-filter/exercise7.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-higher-order-functions-map-filter/exercise7.js
@@ -1,7 +1,18 @@
 const books = require('./obj');
 
+// Para retornar o primeiro nome do livro em que o nome do autor tenha 3 '.' (pontos) no inicio;
+
 const authorWith3Dots = () => {
   return books.find((book) => (book.author.name.split(' ').filter((word) => word.endsWith('.')).length === 3)).name;
 }
 
 console.log(authorWith3Dots());
+
+// Para retornar o nome de todos os livros em que o nome do Autor tenha 3 '.' (pontos) no inicio;
+
+function authorWith3DotsOnName() {
+// escreva seu código aqui
+  return books.filter((item) => /.*\. .*\. .*\./.test(item.author.name)).map((element) => element.name);
+};
+
+console.log(authorWith3DotsOnName());
